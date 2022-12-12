02263b85446d5358867f557eba5e3320a19995e3
shandie <shancarl.belgica.3@gmail.com>
2022-12-12 11:10:06 +0800

    Fixed bugs

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,6 +1,10 @@
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
+
+const slugify = require('slugify');
+
+const replaceTemplate = require('./modules/replaceTemplate');
 //Files_________________________________
 //const fs = require('fs');
 
@@ -28,21 +32,6 @@ const fs = require('fs');
 // console.log('Read file success'); const http = require('http); const url = require('url)
 
 //Server_________________________________
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-
-  return output;
-};
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   'utf-8'
@@ -93,7 +82,7 @@ const server = http.createServer((req, res) => {
     //NOT FOUND
     default:
       res.writeHead(404, { 'Content-type': 'text/html' });
-      res.end('<h1>404: Page Not Found</h1>');
+      res.end('<h1>404: Page Not Found.</h1>');
       break;
   }
 });
