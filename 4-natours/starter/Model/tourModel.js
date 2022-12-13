889fcb23b84e85a91722640f718998bead080904
shandie <shancarl.belgica.3@gmail.com>
2022-12-13 23:34:39 +0800

    Added sort feature

--- 4-natours/starter/Model/tourModel.js
+++ 4-natours/starter/Model/tourModel.js
@@ -15,7 +15,7 @@ const tourSchema = new mongoose.Schema({
     type: Number,
     required: [true, 'A tour must have a group size'],
   },
-  ratingAverage: {
+  ratingsAverage: {
     type: Number,
     default: 4.5,
   },
