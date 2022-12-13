1697dc3227edcb19c8537aa61e0f233c9af55173
shandie <shancarl.belgica.3@gmail.com>
2022-12-13 15:41:38 +0800

    Establish MongoDB Connection

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,6 +1,17 @@
+const mongoose = require('mongoose');
+
+const env = require('dotenv');
+
+env.config({ path: './.env' });
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.PASSWORD);
+
 const app = require('./app');
+
+mongoose.set('strictQuery', true);
+mongoose.connect(DB).then(() => console.log('DB Connection Established'));
 //SERVER
-const port = 3000;
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
 });
